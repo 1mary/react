9855ad3a01246678b3a068b28d25de80fae325ac
1mary <tyuleneva-1998@mail.ru>
2024-03-10 23:06:38 +0300

    update header

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import style from './Logo.module.css';
 // import logo from "./img/clever-logo.png";
-import logo from "./img/logo2.png";
+import logo from "./img/logonew.png";
 const Logo = () => {
     return (
             <div className={`${style.blockLogo} headerBlock`}>
