a6ea52c52c9e96dd755d96bdde4e499041c25edd
KosmyninaMarina <kosmynina.els@gmail.com>
2024-03-19 12:24:05 +0300

    messages

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -2,8 +2,18 @@ import React from "react";
 import style from './Messages.module.css';
 
 const Messages = () => {
-    return <div className="">
-                dialog
+    return <div className={style.dialogs}>
+                <div className={style.dialogsItems}>
+                    <div className={style.dialog + ' '+ style.active}>DanЯ</div>
+                    <div className={style.dialog}>Andrey</div>
+                    <div className={style.dialog}>Sasha</div>
+                    <div className={style.dialog}>Viktor</div>
+                </div>
+                <div className={style.messages}>
+                    <div className={style.message}>Я прилетел в Питер</div>
+                    <div className={style.message}>Привет</div>
+                    <div className={style.message}>yo</div>
+                </div>                    
             </div>
 }
 
